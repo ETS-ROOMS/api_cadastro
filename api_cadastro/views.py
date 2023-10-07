d5b96ca1d38e622bb08bc5c8387d010db7fe99de
viktormarinho <viktormpcs@gmail.com>
2023-10-07 14:40:49 -0300

    get day from querystring in eventos list

--- api_cadastro/views.py
+++ api_cadastro/views.py
@@ -5,9 +5,11 @@ from rest_framework.response import Response
 
 # Create your views here.
 
+
 class cad_instrutorViewset(viewsets.ModelViewSet):
     queryset = cad_instrutor.objects.all()
     serializer_class = cad_instrutorSerializer
+
 
 class cad_salaViewset(viewsets.ModelViewSet):
     queryset = cad_sala.objects.all()
@@ -18,9 +20,16 @@ class cad_salaViewset(viewsets.ModelViewSet):
         serializer = cad_salaSerializer(queryset, many=True)
         return Response(serializer.data)
 
+
 class EventoViewset(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
+
+    def list(self, request):
+        day = request.query_params.get('day')
+        queryset = Evento.objects.filter(data_inicio=day, data_fim=day)
+        serializer = EventoSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class ImagemViewset(viewsets.ModelViewSet):
     queryset = Imagem.objects.all()
